d85cc7093ef2a9c80051ad563dc1ad4043261a41
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-12-17 15:11:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
-import json
+from data_base.src.db_handler import DatabaseHandler
 
-from file_work.src.file_processor import FileProcessor
-from data_base.connection.db_connection import DatabaseConnection
 
 def main():
-    json_data = {'name': 'Alice',
-                 'age': 30,
-                 'city': 'New York'}
-
-    path_params = {
-        'folder_path': './data',
-        'file_name': 'data',
-        'extension': 'json'
-    }
-
-    # FileProcessor.write_file(path_params, json_data, overwrite=True)
-
-    data = FileProcessor.read_file(path_params)
-    print("Read data:", data)
-
-    db_path_params = {
+    db_path = {
         'folder_path': './data',
         'file_name': 'database',
         'extension': 'db'
     }
 
-    db_connection = DatabaseConnection(db_path_params)
-    db_connection.close()
+    db_handler = DatabaseHandler(db_path, db_path["folder_path"])
 
-    # path_params = {
-    #     'folder_path': './data',
-    #     'file_name': 'binary_file',
-    #     'extension': 'bin'
-    # }
-    #
-    # json_string = json.dumps(json_data)
-    # FileProcessor.write_file(path_params, json_string, overwrite=True)
-    #
-    # data = FileProcessor.read_file(path_params)
-    # json_data_new = json.loads(data)
-    # print("Read binary data:", json_data_new)
+    try:
+        db_handler.setup()
+        # db_handler.run()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        db_handler.cleanup()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
