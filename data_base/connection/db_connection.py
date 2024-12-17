c3c001903b19bb511688d18894a3336cd72371b3
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-12-17 15:10:09 +0700

    класс для соединения с бд

--- data_base/connection/db_connection.py
+++ data_base/connection/db_connection.py
@@ -1,7 +1,7 @@
+import os
 import sqlite3
 
 from file_work.path_validator.path_validator import PathValidator
-from file_work.utils.utilities import Check
 from ..utils.utilities import Messages
 
 class DatabaseConnection:
@@ -9,49 +9,80 @@ class DatabaseConnection:
         self.path_params = path_params
         self.db_path = None
         self.connection = None
+
         self.extract_db_path()
 
+    def extract_db_path(self):
+        try:
+            folder_path, file_name, extension = PathValidator.extract_path(self.path_params)
+            self.db_path = PathValidator.get_full_path(folder_path, file_name, extension)
+        except Exception as e:
+            Messages.Error.try_action(e)
+            Messages.Error.db_path()
+
     def connect(self):
-        if not PathValidator.for_read(self.path_params.get('folder_path', ''),
-                                      self.path_params.get('file_name', ''),
-                                      self.path_params.get('extension', ''),
-                                      ['db']):
+        if self.connection:
+            Messages.Error.connection_already_established(self.db_path)
+            return
+
+        try:
+            PathValidator.for_read(self.path_params.get('folder_path', ''),
+                                   self.path_params.get('file_name', ''),
+                                   self.path_params.get('extension', ''),
+                                   ['db'])
+        except Exception as e:
+            Messages.Error.try_action(e)
             Messages.Error.connection(self.db_path)
             return
 
         try:
             self.connection = sqlite3.connect(self.db_path)
+            Messages.Complete.connection(self.db_path)
         except Exception as e:
             Messages.Error.try_action(e)
+            Messages.Error.connection(self.db_path)
             return
-
-        Messages.Complete.connection(self.db_path)
-
-    def create_db(self):
-        try:
-            self.connection = sqlite3.connect(self.db_path)
-        except Exception as e:
-            Messages.Error.try_action(e)
-            return
-
-        Messages.Complete.connection(self.db_path)
 
     def close(self):
         if self.connection:
-            self.connection.close()
-            Messages.Complete.close_connection(self.db_path)
+            try:
+                self.connection.close()
+                self.connection = None
+                Messages.Complete.close_connection(self.db_path)
+            except Exception as e:
+                Messages.Error.try_action(e)
         else:
             Messages.Error.miss_connection(self.db_path)
 
-    def extract_db_path(self):
-        specified_keys = ["folder_path", "file_name", "extension"]
+    def create_db(self):
+        self.close()
+        self.connection = None
 
-        if not Check.dictionary_specified(self.path_params, specified_keys):
-            Messages.Error.specified_dictionary(specified_keys)
-            return None
+        if os.path.exists(self.db_path):
+            Messages.Error.db_already_exist(self.db_path)
+            Messages.Error.create_db(self.db_path)
+        else:
+            try:
+                self.connection = sqlite3.connect(self.db_path)
+                Messages.Complete.create_db(self.db_path)
+                Messages.Complete.connection(self.db_path)
+            except Exception as e:
+                Messages.Error.try_action(e)
+                Messages.Error.create_db(self.db_path)
+                Messages.Error.connection(self.db_path)
+                return
 
-        folder_path = self.path_params.get('folder_path', '')
-        file_name = self.path_params.get('file_name', '')
-        extension = self.path_params.get('extension', '')
+    def delete_db(self):
+        self.close()
+        self.connection = None
 
-        self.db_path = PathValidator.get_full_path(folder_path, file_name, extension)
+        if os.path.exists(self.db_path):
+            try:
+                os.remove(self.db_path)
+                Messages.Complete.delete_db(self.db_path)
+            except Exception as e:
+                Messages.Error.try_action(e)
+                Messages.Error.delete_db(self.db_path)
+        else:
+            Messages.Error.db_file_found(self.db_path)
+            Messages.Error.delete_db(self.db_path)
