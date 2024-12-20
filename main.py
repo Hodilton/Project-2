895a124805999da5b021ad108e923d035f9fc4a2
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-12-20 08:55:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,36 @@ from data_base.src.db_handler import DatabaseHandler
 from app.app_main import Application
 
 from app.displays.display_base import DisplayBase
+from app.displays.display_room import DisplayRoom
+from app.displays.display_room_class import DisplayRoomClass
 
 def main():
-    db_path = {
-        'folder_path': './data',
-        'file_name': 'database',
-        'extension': 'db'
-    }
-
-    db_handler = DatabaseHandler(db_path, db_path["folder_path"])
+    db_handler = DatabaseHandler(config_path="./data")
 
     try:
         frames_config = {
             "Аудитории": {
                 "room_class": {
-                    "class": DisplayBase,
-                    "name": "Аудитории",
+                    "class": DisplayRoomClass,
+                    "name": "Кабинет - места",
                 },
-            },
-            "Сотрудники": {
-                "staff": {
-                    "class": DisplayBase,
-                    "name": "Сотрудники",
+
+                "room": {
+                    "class": DisplayRoom,
+                    "name": "Аудитории",
                 },
             },
         }
 
-        # db_handler.setup()
+        db_handler.setup()
+        # db_handler.run()
+
         app = Application(frames_config, db_handler.manager)
         app.run()
-        # db_handler.run()
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
-        # db_handler.cleanup()
-        pass
+        db_handler.cleanup()
 
 if __name__ == "__main__":
     main()
