6c4fcabdd040b550dfde851594ad2af705bb5270
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-12-20 16:37:08 +0700

    Update db_handler.py

--- data_base/src/db_handler.py
+++ data_base/src/db_handler.py
@@ -2,6 +2,14 @@ from ..connection.db_connection import DatabaseConnection
 from ..table_config.table_config_loader import TableConfigLoader
 from .db_manager import DatabaseManager
 
+'''
+    self.manager.tables['genres'].insert_data(("Ужасы", ))
+    self.manager.tables['genres'].update_data(("Ужасы", ), (1,))
+    self.manager.tables['genres'].delete_data((1,))
+    data = self.manager.tables['genres'].find_id(("Ужасы", )) 
+    data = self.manager.tables['genres'].fetch_one((1,))
+    data = self.manager.tables['genres'].fetch_all()
+'''
 
 class DatabaseHandler:
     def __init__(self, config_path):
@@ -21,27 +29,76 @@ class DatabaseHandler:
         self.manager = DatabaseManager(self.connection, tables_config_loader)
         self.manager.init_tables()
 
-    def run(self):
+    def seed(self):
+        # self.manager.drop_tables()
         self.manager.create_tables()
 
-        # self.manager.tables['room_class'].insert_data(("507", 20))
-        # self.manager.tables['room_class'].update_data(("507", 22), (3,))
+        # self.manager.tables['genres'].insert_data(("Художественная литература",))
+        # self.manager.tables['genres'].insert_data(("Научная литература",))
+        # self.manager.tables['genres'].insert_data(("Фантастика",))
+        # self.manager.tables['genres'].insert_data(("Фэнтези",))
+        # self.manager.tables['genres'].insert_data(("Детектив",))
+        # self.manager.tables['genres'].insert_data(("Биография",))
+        # self.manager.tables['genres'].insert_data(("История",))
+        # self.manager.tables['genres'].insert_data(("Детская литература",))
+        # self.manager.tables['genres'].insert_data(("Романтика",))
+        # self.manager.tables['genres'].insert_data(("Ужасы",))
+        #
+        # self.manager.tables['authors'].insert_data(("Пушкин", "Александр", "Сергеевич"))
+        # self.manager.tables['authors'].insert_data(("Толстой", "Лев", "Николаевич"))
+        # self.manager.tables['authors'].insert_data(("Достоевский", "Фёдор", "Михайлович"))
+        # self.manager.tables['authors'].insert_data(("Гоголь", "Николай", "Васильевич"))
+        # self.manager.tables['authors'].insert_data(("Тургенев", "Иван", "Сергеевич"))
+        # self.manager.tables['authors'].insert_data(("Булгаков", "Михаил", "Афанасьевич"))
+        # self.manager.tables['authors'].insert_data(("Чехов", "Антон", "Павлович"))
+        # self.manager.tables['authors'].insert_data(("Есенин", "Сергей", "Александрович"))
+        # self.manager.tables['authors'].insert_data(("Блок", "Александр", "Александрович"))
+        # self.manager.tables['authors'].insert_data(("Маяковский", "Владимир", "Владимирович"))
 
-        # self.manager.tables['room_class'].delete_data((6,))
+        # self.manager.tables['book_statuses'].insert_data(("Выдана",))
+        # self.manager.tables['book_statuses'].insert_data(("Возвращена",))
+        # self.manager.tables['book_statuses'].insert_data(("Зарезервирована",))
+        # self.manager.tables['book_statuses'].insert_data(("Утеряна",))
 
-        # data = self.manager.tables['room_class'].find_id(("507", ))
-        # print("Data:", data)
+        # self.manager.tables['books'].insert_data(("Евгений Онегин", 1, 1, 1833))
+        # self.manager.tables['books'].insert_data(("Война и мир", 1, 2, 1869))
+        # self.manager.tables['books'].insert_data(("Преступление и наказание", 1, 3, 1866))
+        # self.manager.tables['books'].insert_data(("Мёртвые души", 1, 4, 1842))
+        # self.manager.tables['books'].insert_data(("Отцы и дети", 1, 5, 1862))
+        # self.manager.tables['books'].insert_data(("Мастер и Маргарита", 1, 6, 1940))
+        # self.manager.tables['books'].insert_data(("Вишнёвый сад", 1, 7, 1904))
+        # self.manager.tables['books'].insert_data(("Анна Каренина", 1, 2, 1877))
+        # self.manager.tables['books'].insert_data(("Доктор Живаго", 1, 2, 1957))
+        # self.manager.tables['books'].insert_data(("Двенадцать", 1, 8, 1918))
 
-        # data = self.manager.tables['room_class'].fetch_one((5,))
-        # print("Data:", data)
+        self.manager.tables['readers'].insert_data(("Иванов", "Иван", "Иванович"))
+        self.manager.tables['readers'].insert_data(("Петров", "Петр", "Сергеевич"))
+        self.manager.tables['readers'].insert_data(("Сидоров", "Алексей", "Николаевич"))
+        self.manager.tables['readers'].insert_data(("Кузнецов", "Дмитрий", "Викторович"))
+        self.manager.tables['readers'].insert_data(("Смирнова", "Мария", "Александровна"))
+        self.manager.tables['readers'].insert_data(("Попова", "Елена", "Сергеевна"))
+        self.manager.tables['readers'].insert_data(("Васильев", "Игорь", "Павлович"))
+        self.manager.tables['readers'].insert_data(("Козлова", "Анна", "Михайловна"))
+        self.manager.tables['readers'].insert_data(("Зайцева", "Ольга", "Ивановна"))
+        self.manager.tables['readers'].insert_data(("Морозов", "Никита", "Сергеевич"))
 
-        # data = self.manager.tables['room_class'].fetch_all()
-        # print("Data:", data)
+        self.manager.tables['departments'].insert_data(("Обслуживание",))
+        self.manager.tables['departments'].insert_data(("Читальный зал",))
+        self.manager.tables['departments'].insert_data(("Абонемент",))
+        self.manager.tables['departments'].insert_data(("Архив",))
+        self.manager.tables['departments'].insert_data(("Информационный отдел",))
 
-        # self.manager.tables['room'].insert_data(("Аудитория 1", 7))
+        self.manager.tables['librarians'].insert_data(("Ковалёв", "Евгений", "Александрович", 1))
+        self.manager.tables['librarians'].insert_data(("Михайлова", "Ольга", "Викторовна", 2))
+        self.manager.tables['librarians'].insert_data(("Семенова", "Ирина", "Сергеевна", 3))
+        self.manager.tables['librarians'].insert_data(("Воронов", "Андрей", "Петрович", 4))
+        self.manager.tables['librarians'].insert_data(("Павлова", "Татьяна", "Николаевна", 5))
 
-        # data = self.manager.tables['room'].fetch_all()
-        # print("Data:", data)
+        self.manager.tables['borrowed_books'].insert_data(("2024-01-01", "2024-01-15", 1, 1, 1, 1))
+        self.manager.tables['borrowed_books'].insert_data(("2024-01-02", "2024-01-16", 1, 2, 2, 2))
+        self.manager.tables['borrowed_books'].insert_data(("2024-01-03", "2024-01-17", 2, 3, 3, 3))
+        self.manager.tables['borrowed_books'].insert_data(("2024-01-04", "2024-01-18", 3, 4, 4, 4))
+        self.manager.tables['borrowed_books'].insert_data(("2024-01-05", "2024-01-19", 4, 5, 5, 5))
 
     def cleanup(self):
         # self.manager.drop_tables()
